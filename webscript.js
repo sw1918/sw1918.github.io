f00b23b0aa3a65eac4542f57b4eb56081fdd067c
sw1918 <spencer1918@live.com>
2020-05-13 16:33:28 -0700

    add pubs page, update cv

--- webscript.js
+++ webscript.js
@@ -1,6 +1,7 @@
 n = document.getElementById('name').offsetWidth;
 h = document.getElementById('home').offsetWidth;
 p = document.getElementById('proj').offsetWidth;
+pu = document.getElementById('pubs').offsetWidth;
 t = document.getElementById('teach').offsetWidth;
 c = document.getElementById('cv').offsetWidth;
 d = document.getElementById('drop').offsetWidth;
@@ -11,8 +12,10 @@ w0 = n + d;
 w1 = w0 + t;
 //Width of name, teaching, project, and dropdown
 w2 = w1 + p;
-//Width of name, teaching, project, and cv
-w3 = w2 + c - d;
+//Width of name, teaching, project, pubs, and dropdown
+w3 = w1 + pu
+//Width of name, teaching, project, pubs, and cv
+w4 = w3 + c - d;
 
 reloc();
 window.addEventListener("resize", reloc);
@@ -29,23 +32,26 @@ function reloc() {
   //document.getElementById('debug0').innerHTML = w3;
   //document.getElementById('debug1').innerHTML = x;
 
-  if (x > w3 + 150) {
+  if (x > w4 + 150) {
     document.getElementById('cv').style.display = 'block';
     document.getElementById('drop').style.display = 'none';
+    document.getElementById('pubs').style.display = 'block';
     document.getElementById('proj').style.display = 'block';
     document.getElementById('teach').style.display = 'block';
     document.getElementById('home').style.display = 'block';
   }
 
-  if (x <= w3 + 150 && x > w2 + 50) {
+  if (x <= w4 + 150 && x > w3 + 50) {
     document.getElementById('cv').style.display = 'none';
     document.getElementById('drop').style.display = 'block';
+    document.getElementById('pubs').style.display = 'block';
     document.getElementById('proj').style.display = 'block';
     document.getElementById('teach').style.display = 'block';
     document.getElementById('home').style.display = 'block';
 
     document.getElementById('homed').style.display = 'none';
     document.getElementById('teachd').style.display = 'none';
+    document.getElementById('pubsd').style.display = 'none';
     document.getElementById('projd').style.display = 'none';
 
     if (document.getElementById('cv').className == "current") {
@@ -57,15 +63,17 @@ function reloc() {
 
   }
   
-  if (x <= w2 + 150 && x > w1 + 50) {
+  if (x <= w3 + 150 && x > w2 + 50) {
     document.getElementById('cv').style.display = 'none';
     document.getElementById('drop').style.display = 'block';
-    document.getElementById('proj').style.display = 'none';
+    document.getElementById('pubs').style.display = 'none';
+    document.getElementById('proj').style.display = 'block';
     document.getElementById('teach').style.display = 'block';
     document.getElementById('home').style.display = 'block';
 
     document.getElementById('homed').style.display = 'none';
     document.getElementById('teachd').style.display = 'none';
+    document.getElementById('pubsd').style.display = 'block';
     document.getElementById('projd').style.display = 'block';
 
     if (document.getElementById('proj').className == "current") {
@@ -76,21 +84,44 @@ function reloc() {
     }
   }
 
-    if (x <= w1 + 150 && x > w0 + 50) {
+  if (x <= w2 + 150 && x > w1 + 50) {
     document.getElementById('cv').style.display = 'none';
     document.getElementById('drop').style.display = 'block';
+    document.getElementById('pubs').style.display = 'none';
+    document.getElementById('proj').style.display = 'none';
+    document.getElementById('teach').style.display = 'block';
+    document.getElementById('home').style.display = 'block';
+
+    document.getElementById('homed').style.display = 'none';
+    document.getElementById('teachd').style.display = 'none';
+    document.getElementById('pubsd').style.display = 'block';
+    document.getElementById('projd').style.display = 'block';
+
+    if (document.getElementById('proj').className == "current") {
+      document.getElementById('btn').style.backgroundColor = "#42464d"; 
+    }
+    if (document.getElementById('teach').className == "current") {
+      document.getElementById('btn').style.backgroundColor = "#36393f"; 
+    }
+  }
+
+  if (x <= w1 + 150 && x > w0 + 50) {
+    document.getElementById('cv').style.display = 'none';
+    document.getElementById('drop').style.display = 'block';
+    document.getElementById('pubs').style.display = 'none';
     document.getElementById('proj').style.display = 'none';
     document.getElementById('teach').style.display = 'none';
     document.getElementById('home').style.display = 'block';
 
     document.getElementById('homed').style.display = 'none';
     document.getElementById('teachd').style.display = 'block';
+    document.getElementById('pubsd').style.display = 'block';
     document.getElementById('projd').style.display = 'block';
 
-    if (document.getElementById('teach').className == "current") {
+    if (document.getElementById('proj').className == "current") {
       document.getElementById('btn').style.backgroundColor = "#42464d"; 
     }
-    if (document.getElementById('home').className == "current") {
+    if (document.getElementById('teach').className == "current") {
       document.getElementById('btn').style.backgroundColor = "#36393f"; 
     }
   }
@@ -98,12 +129,14 @@ function reloc() {
     if (x <= w0 + 150) {
     document.getElementById('cv').style.display = 'none';
     document.getElementById('drop').style.display = 'block';
+    document.getElementById('pubs').style.display = 'none';
     document.getElementById('proj').style.display = 'none';
     document.getElementById('teach').style.display = 'none';
     document.getElementById('home').style.display = 'none';
 
     document.getElementById('homed').style.display = 'block';
     document.getElementById('teachd').style.display = 'block';
+    document.getElementById('pubsd').style.display = 'block';
     document.getElementById('projd').style.display = 'block';
 
     if (document.getElementById('home').className == "current") {
